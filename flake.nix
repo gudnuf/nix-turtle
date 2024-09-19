b4517d3daa2cb0874c100b3cdd6e373adcea0661
gudnuf <gudnuf21@proton.me>
2024-09-19 17:42:13 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "NixOS configuration for turtle with node2nix-generated holesail";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    holesail-flake.url = "github:gudnuf/holesail/flake";
+  };
+
+  outputs = { self, nixpkgs, holesail-flake }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+#      holesail = (import ./packages/holesail {
+ #       inherit pkgs system;
+  #      nodejs = pkgs.nodejs_22;
+   #   }).package;
+    in {
+      nixosConfigurations = {
+        turtle = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+
+            # Use holesail package from the Holesail flake
+            {
+              environment.systemPackages = with pkgs; [
+                holesail-flake.packages.${system}.holesail
+              ];
+            }
+          ];
+        };
+      };
+    };
+
+}
---
+++ home.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, user, ... }:
+
+{
+  home = {
+    username = user;
+    homeDirectory = "/home/${user}";
+    stateVersion = "24.05";
+    packages = with pkgs; [
+      holesail
+    ];
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+    sessionPath = [ "${pkgs.holesail}/bin" ];
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "gudnuf";
+    userEmail = "gudnuf21@proton.me";
+  };
+
+  programs.home-manager.enable = true;
+}
